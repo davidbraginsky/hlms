d91f868cad952cb1ac9721af8d2ababb45c9126f
David <braginsky.david@yandex.ru>
2021-03-18 21:54:11 +0300

    - added style.css - added config and todo - added logo - updated index.html with basic structure

---
+++ config.js
@@ -0,0 +1,29 @@
+/*
+COLORS
+- light grey        #EAE5FC
+- purple            #9560F9
+- pink              #E79BE3
+- blue/grey         #9F9CEF
+- blue              #7696F8
+- dark grey         #A3A7BE
+- blue/pink         #705AD1
+
+
+
+-sidebar            rgba(248,243,255,255)
+-main background    rgba(238,243,254,255)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
---
+++ todo.js
@@ -0,0 +1,30 @@
+/*
+
+v. 0.1                      // 18.03.2021
+
+    - create basic structure of webpage
+        - sidebar
+        - blocks
+
+
+    - decide which blocks will be on homepage
+    - decide on functionality
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
